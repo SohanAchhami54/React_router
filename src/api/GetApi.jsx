f38ce191b9833bda98bda3f1a0b6ce929f228430
Sohan Ac <sohanachhami655@gmail.com>
2025-05-24 07:56:04 +0545

    day_17_api_secure

--- src/api/GetApi.jsx
+++ src/api/GetApi.jsx
@@ -1,7 +1,9 @@
 export const GetMoviesData=async()=>{
 
     try{
-       const response= await fetch("https://www.omdbapi.com/?s=John%20Wick&apikey=96129abc");
+    //    const response= await fetch("https://www.omdbapi.com/?s=John%20Wick&apikey=96129abc");
+    const response = await fetch(`https://www.omdbapi.com/?s=John%20Wick&apikey=${import.meta.env.VITE_API_KEY}`);
+
        const jr=response.json();
     //    console.log(jr);
        return jr;
--- src/components/UI/Card.jsx
+++ src/components/UI/Card.jsx
@@ -10,23 +10,12 @@ export const Card=({curElem})=>{
         </section>
         <section className="flex gap-2 justify-around mt-2 font-medium border-2 bg-black
          text-amber-50 rounded-2xl mx-1 ">
-            {/* <h1>{curElem.imdbID} </h1> */}
             <h1>{curElem.Year}</h1>
             <h1>{curElem.Title}</h1>
         </section>
        
-      </div>
-        {/* <section className="grid grid-cols-3">
-            <section>
-          <img src={Poster} alt="" />
-            </section>
-            <section>
-                 <p>{imdbID} </p>
-            </section>
-       
-        </section>
-    */}
-     {/* <h2>{navlink} </h2> */}
+      </div> 
+      
         </>
     );
 };
